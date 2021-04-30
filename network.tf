f3744d6be534178033f81c433663dcdd371bc03a
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 08:16:17 +0900

    プライベートサブネット作成

--- network.tf
+++ network.tf
@@ -43,3 +43,11 @@ resource "aws_subnet" "public_A1" {
     Name = "${var.sysname}-prod-web-subnetA1"
   }
 }
+
+# Private subnet
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
