8969d42199eed94e13040a14ff7d20aa837ccdec
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-04-30 20:30:06 +0900

    パブリックサブネット作成

--- network.tf
+++ network.tf
@@ -8,3 +8,38 @@ resource "aws_vpc" "main" {
     Name = "${var.sysname}-prod-vpc"
   }
 }
+
+# Internet gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route config
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Route table association
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_A1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Public subnet
+resource "aws_subnet" "public_A1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "${var.sysname}-prod-web-subnetA1"
+  }
+}
