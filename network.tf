4e3db1276d24b28cc8da1c07d2617bac820bb298
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 08:41:34 +0900

    プライベートサブネット用ルート作成

--- network.tf
+++ network.tf
@@ -37,6 +37,12 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
 # Route table association
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
