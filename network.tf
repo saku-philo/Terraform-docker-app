08928413d3256b9953492cc7da402f5690788bfd
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 08:49:23 +0900

    パブリックサブネットのマルチAZ化

--- network.tf
+++ network.tf
@@ -54,6 +54,16 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 # Public subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
@@ -65,6 +75,22 @@ resource "aws_subnet" "public" {
     Name = "${var.sysname}-prod-web-subnetA1"
   }
 }
+
+# Public MultiAZ
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+}
+
 
 # Private subnet
 resource "aws_subnet" "private" {
