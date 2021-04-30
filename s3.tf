0573b125031417d6f635176f6b16b391cc0f2475
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-04-30 19:11:37 +0900

    s3 ログバケット作成

--- s3.tf
+++ s3.tf
@@ -26,12 +26,43 @@ resource "aws_s3_bucket_public_access_block" "private" {
 # public bucket
 resource "aws_s3_bucket" "public" {
   bucket = "tf-docker-app-public"
-  acl = "public-read"
+  acl    = "public-read"
 
   cors_rule {
     allowed_origins = ["https://www.example.com"]
     allowed_methods = ["GET"]
     allowed_headers = ["*"]
     max_age_seconds = 3000
+  }
+}
+
+# log bucket
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "tf-docker-app-alb-log"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["582318560864"]
+    }
   }
 }
