8c5fde35889afd346e0687f39fa03fa1f7f5abe4
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 08:28:26 +0900

    プライベートサブネット用ルートテーブル作成

--- network.tf
+++ network.tf
@@ -19,6 +19,10 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
 # Route config
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
@@ -28,12 +32,17 @@ resource "aws_route" "public" {
 
 # Route table association
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public_A1.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
 # Public subnet
-resource "aws_subnet" "public_A1" {
+resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
