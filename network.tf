d41d1d12dd302fb67ae23239cc54f495849d7470
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 08:31:56 +0900

    EIP, NATゲートウェイ作成

--- network.tf
+++ network.tf
@@ -14,6 +14,13 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
+# NAT gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.main]
+}
+
 # Route table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -59,4 +66,10 @@ resource "aws_subnet" "private" {
   cidr_block              = "10.0.64.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
+}
+
+# EIP
+resource "aws_eip" "nat_gateway" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.main]
 }
