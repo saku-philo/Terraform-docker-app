2e277f5c3cafb01dd54abb90d7a63c8fa5a448b7
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-03 08:55:40 +0900

    インバウンドルール作成

--- security_group/main.tf
+++ security_group/main.tf
@@ -2,10 +2,19 @@ variable "name" {}
 variable "vpc_id" {}
 variable "port" {}
 variable "cidr_blocks" {
-  type = list(string)
+  type = list(string) # 文字列型を指定
 }
 
 resource "aws_security_group" "default" {
   name = var.name
   vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "ingress" {
+  type = "ingress"
+  from_port = var.port
+  to_port = var.port
+  protcol = "tcp"
+  cidr_blocks = var.cidr_blocks
+  security_group_id = aws_security_group.default.id
 }
