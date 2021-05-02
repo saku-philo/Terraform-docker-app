bf0b4a17a4de81eb35f8da24d213d1d89ecd1918
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-03 08:47:58 +0900

    セキュリティグループモジュール作成

---
+++ security_group/main.tf
@@ -0,0 +1,11 @@
+variable "name" {}
+variable "vpc_id" {}
+variable "port" {}
+variable "cidr_blocks" {
+  type = list(string)
+}
+
+resource "aws_security_group" "default" {
+  name = var.name
+  vpc_id = var.vpc_id
+}
