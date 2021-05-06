084a106155359cdaff140828436e1c21a68ca45c
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-06 12:32:51 +0900

    CloudWatch Logs設定: ロググループ、IAMロール定義

--- ecs.tf
+++ ecs.tf
@@ -16,7 +16,8 @@ resource "aws_ecs_task_definition" "web_server_task" {
   container_definitions    = file("./container_definitions.json")
 }
 
-# 3. ECSサービスの定義
+# 3. ECSサービス
+# 3-1. ECSサービスの定義
 resource "aws_ecs_service" "web_server_service" {
   name                              = "web_server_service"
   cluster                           = aws_ecs_cluster.web_server.arn
@@ -47,11 +48,24 @@ resource "aws_ecs_service" "web_server_service" {
   }
 }
 
-#　4. nginxモジュール
+#　3-2. nginxセキュリティグループモジュール
 module "nginx_sg" {
   source      = "./security_group"
   name        = "nginx_sg"
   vpc_id      = aws_vpc.main.id
   port        = 80
   cidr_blocks = [aws_vpc.main.cidr_block]
+}
+
+# 4. CloudWatch Logs関係の定義
+# 4-1. ロググループ定義
+resource "aws_cloudwatch_log_group" "ecs_web_server" {
+  name = "/ecs/web_server"
+  retention_in_days = 180
+}
+
+# 4-2. ECSタスク実行IAMロール
+data "aws_ima_policy" "ecs_task_execution_role_policy" {
+  # AWS管理ポリシー使用
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
