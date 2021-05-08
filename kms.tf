27e685c1348422f4a5160f1459e979ea0380eed2
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-08 12:51:29 +0900

    アプリ名修正

--- kms.tf
+++ kms.tf
@@ -1,6 +1,10 @@
-resource "aws_kms_key" "tfdoc" {
-  description = "tfdoc app Customer Master Key"
-  enable_key_rotation = true
-  is_enabled = true
+# ===============================================
+# KMS
+# ===============================================
+# 1. KMS
+resource "aws_kms_key" "tfdock" {
+  description             = "tfdock app Customer Master Key"
+  enable_key_rotation     = true
+  is_enabled              = true
   deletion_window_in_days = 30
 }
