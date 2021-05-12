8989326dfc13458c31724dbe9876e5b0c5c36f5d
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-12 11:03:46 +0900

    EC2インスタンス、ユーザーデータ定義

--- ops_server.tf
+++ ops_server.tf
@@ -42,3 +42,16 @@ resource "aws_iam_instance_profile" "ec2_for_ssm" {
   name = "ec2-for-ssm"
   role = module.ec2_for_ssm_role.iam_role_name
 }
+
+# 4. EC2インスタンス
+resource "aws_instance" "tfdock_for_operation" {
+  ami                  = "ami-0c3fd0f5d33134a76"
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id            = aws_subnet.private_1.id
+  user_data            = file("./user_data.sh")
+}
+
+output "operation_instance_id" {
+  value = aws_instance.tfdock_for_operation.id
+}
